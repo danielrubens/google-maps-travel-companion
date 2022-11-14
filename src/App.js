89c11429390841a8e78a790113a09e22a38d86a1
danielrubens <danielrubensdaniel@gmail.com>
2022-11-14 17:03:13 -0300

    style setted up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react"
+import {Header, List, Map} from './components'
+import { CssBaseline, Grid } from "@material-ui/core"
 
 const App = () => {
     return(
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+        <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={ { width: '100%' } }>
+            <Grid item xs={12} md={4}>
+                <List />
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Map />
+            </Grid>
+        </Grid>
+        </>
     )
 }
 
